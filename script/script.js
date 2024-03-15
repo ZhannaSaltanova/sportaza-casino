c9c2a51a72cbd73c0218062f3f98e088cc0b3fc8
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-15 12:46:39 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -6,8 +6,22 @@ $(document).ready(function() {
   });
 });
 
-document.querySelector('.menu').addEventListener('click', function(e) {
-    e.preventDefault(); 
-    this.classList.toggle('active'); 
-    document.querySelector('.text-second').classList.toggle('shift-right'); 
-  });
+window.onscroll = function() {
+  let sidebar = document.querySelector('.sidebar');
+  let footer = document.querySelector('footer');
+  let header = document.querySelector('header'); 
+  
+  
+  let headerHeight = header.offsetHeight;
+  let scrollY = window.scrollY;
+  let footerTop = footer.offsetTop;
+  let sidebarHeight = sidebar.offsetHeight;
+  
+  if (scrollY + headerHeight + sidebarHeight > footerTop) {
+    sidebar.style.position = 'absolute';
+    sidebar.style.top = (footerTop - sidebarHeight) + 'px';
+  } else {
+    sidebar.style.position = 'absolute';
+    sidebar.style.top = scrollY + headerHeight + 'px';
+  }
+};
